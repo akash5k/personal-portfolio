8666f733d9b45f5830758a5ccd36da61ce392636
akash5k <akashparida185@gmail.com>
2024-05-02 00:46:08 +0530

    fix: typo in footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className="w-full min-h-screen flex items-center justify-center bg-black">
       <div className="md:w-2/3 w-full px-4 text-white flex flex-col">
         <div className="w-full text-6xl font-bold">
-          <h1 className="w-full md:w-2/3">How can we help you. get in touch</h1>
+          <h1 className="w-full md:w-2/3">How can I help you. get in touch</h1>
         </div>
         <div className="flex mt-8 flex-col md:flex-row md:justify-between">
         <Reveal>
